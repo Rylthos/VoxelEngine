f3a07d8cc53477b1c19caa984ee7030a56511f45
Aaron Danton <aarondanton@rylthos.com>
2025-01-20 00:21:47 +0000

    Added back some world generation instead of grid

--- res/shaders/GenerateBrickmap.comp.glsl
+++ res/shaders/GenerateBrickmap.comp.glsl
@@ -6,6 +6,9 @@
 #extension GL_EXT_shader_atomic_int64 : enable
 #extension GL_GOOGLE_include_directive : require
 
+#include "Hash.other.glsl"
+#include "Noise.other.glsl"
+
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 
 layout(buffer_reference, std430) buffer ColourBuffer {
@@ -42,19 +45,44 @@ void setAir(in uvec3 position) {
     p_Colours.colours[getIndex(position)] = vec4(0., 0., 0., -1.);
 }
 
+float height(vec3 pos)
+{
+    float noiseValue = (16 / 30.) * snoise(pos.xz) +
+            (8 / 30.) * snoise(pos.xz * 2.0) +
+            (4 / 30.) * snoise(pos.xz * 4.0) +
+            (2 / 30.) * snoise(pos.xz * 8.0);
+    // noiseValue *= 10;
+
+    return noiseValue;
+}
+
 void main() {
     uvec3 currentIndex = gl_GlobalInvocationID.xyz;
 
     ivec3 worldPosition = p_WorldPosition + ivec3(currentIndex);
+    vec3 uv = vec3(worldPosition);
+    uv.y -= 40.;
+    uv.y = -(uv.y / 1.5);
 
-    if (p_BrickIndex.y % 4 == 0) {
-        uint sum = currentIndex.x + currentIndex.y + currentIndex.z;
-        if (sum % 5 == 0) {
-            setVoxel(currentIndex, vec3(1.));
-        } else {
-            setAir(currentIndex);
-        }
+    uv.xz /= 50.;
+
+    // float heightValue = height(vec3(worldPosition));
+    float value = height(uv) * 10.;
+
+    if (uv.y < value) {
+        setVoxel(currentIndex, vec3(1.));
     } else {
         setAir(currentIndex);
     }
+
+    // if (p_BrickIndex.y % 4 == 0) {
+    //     uint sum = currentIndex.x + currentIndex.y + currentIndex.z;
+    //     if (sum % 5 == 0) {
+    //         setVoxel(currentIndex, vec3(1.));
+    //     } else {
+    //         setAir(currentIndex);
+    //     }
+    // } else {
+    //     setAir(currentIndex);
+    // }
 }
